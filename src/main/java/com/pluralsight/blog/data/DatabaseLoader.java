e73611429e21a4cfae7dfe0ce99374fc8b941dea
spryntyB <82833049+spryntyB@users.noreply.github.com>
2022-09-07 15:41:34 +0200

    Second

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -21,7 +21,7 @@ public class DatabaseLoader implements ApplicationRunner {
             "Earbuds", "Speakers", "Tripod", "Instant Pot", "Coffee Cup", "Keyboard", "Sunglasses"};
     public List<Post> randomPosts = new ArrayList<>();
     public List<Author> authors = new ArrayList<>();
-
+    @Autowired
     public DatabaseLoader(PostRepository postrep) {
         this.postRepository = postrep;
     }
